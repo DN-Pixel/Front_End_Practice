2573acb81e2b48721f2348ed66ec59f63eec9528
dnazical <david.nazical@ign.fr>
2022-07-25 11:34:08 +0200

    last push for fusion

--- src/TabDataLine.tsx
+++ src/TabDataLine.tsx
@@ -1,12 +1,12 @@
 import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
 
-function getDetails(id : number){
-    console.log(id + " line clicked")
+function getDetails(){
+    console.log(2 + " line clicked")
     //todo call api map details {id}
-    window.location.href="http://localhost:3000/mapdetail"+id;
+    window.location.href="http://localhost:3000/mapdetail"+2;
 }
 
 export default function TabDataLine(props: { elem: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }){
-    console.log(props)
+    console.log(typeof props)
     return (<td onClick={()=>getDetails()}>{props.elem}</td>)
 }
